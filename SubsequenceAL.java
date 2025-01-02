31b312a208a608df61ff820816998577e6366bfd
iyashreyar <yashreyar5143@gmail.com>
2025-01-02 12:32:14 +0530

    new codes added

---
+++ SubsequenceAL.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+public class SubsequenceAL {
+  public static void main(String[] args) {
+    System.out.println(subseqAL("", "abc"));
+  }
+  static ArrayList<String> subseqAL(String p, String up){
+    if (up.isEmpty()){
+      ArrayList<String> list = new ArrayList<>();
+      list.add(p);
+      return list;
+    }
+    char ch = up.charAt(0);
+    ArrayList<String> left = subseqAL(p+ch, up.substring(1));
+    ArrayList<String> right = subseqAL(p, up.substring(1));
+
+    left.addAll(right);
+    return left;
+  }
+}
