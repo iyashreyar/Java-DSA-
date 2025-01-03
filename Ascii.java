0e620d7643553d2c4bcf896e0ab25aea935feaa9
iyashreyar <yashreyar5143@gmail.com>
2025-01-03 21:21:23 +0530

    New codes added

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+
+//print the ascii  value of a character
+
+public class Ascii {
+  public static void main(String[] args) {
+    char ch = 'a';
+    System.out.println((char)(ch + 5));
+  }
+}
+
+//to represent a character 
+//    System.out.println((char)(ch + 0));
