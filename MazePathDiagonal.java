24048ad90ea9f2e1e08e32208a54cdddae60ef7c
iyashreyar <yashreyar5143@gmail.com>
2025-01-08 11:04:23 +0530

    New dsa ques added

---
+++ MazePathDiagonal.java
@@ -0,0 +1,20 @@
+public class MazePathDiagonal {
+    public static void main(String[] args) {
+        diagonal("", 3, 3);
+    }
+    static void diagonal(String p, int r, int c){
+        if (r == 1 && c == 1){
+            System.out.println(p);
+            return;
+        }
+        if (r > 1 && c > 1){
+            diagonal(p+'D', r-1, c-1);
+        }
+        if (r > 1){
+            diagonal(p+'V', r-1, c);
+        }
+        if (c > 1){
+            diagonal(p+'H', r, c-1);
+        }
+    }
+}
