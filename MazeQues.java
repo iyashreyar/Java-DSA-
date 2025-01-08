a746575b7cb3617662be356d1851f91b78289b3b
iyashreyar <yashreyar5143@gmail.com>
2025-01-08 10:12:33 +0530

    New dsa ques added

---
+++ MazeQues.java
@@ -0,0 +1,16 @@
+
+//fint the number of steps to go from a point to another point in 2d array if we can only go in either eight or down direction
+
+public class MazeQues {
+    public static void main(String[] args) {
+        System.out.println(count(3, 3));
+    }
+    static int count(int r, int c){
+        if (r == 1 || c == 1){
+            return 1;
+        }
+        int right = count(r-1, c);
+        int left = count(r, c-1);
+        return right+left;
+    }
+}
