5042d0fe3abc77a770fc5426ec730d34555e2926
iyashreyar <yashreyar5143@gmail.com>
2025-01-12 22:15:17 +0530

    New leetcode ques solved

---
+++ LeetCodeQ33.java
@@ -0,0 +1,20 @@
+public class LeetCodeQ33 {
+    public static void main(String[] args) {
+        String s = "luffy is still joyboy";
+        System.out.println(lengthOfLastWord(s));
+    }
+    static int lengthOfLastWord(String s) {
+        /*
+        String[] words = s.trim().split("\\s+");
+        return words[words.length-1].length();
+         */
+
+        //OR
+
+        //s.trim() function : Removes any leading and trailing whitespace from the string s
+        //String s = "  Hello World  ";
+        //Output s = "Hello World"
+
+        return s.trim().length() - s.trim().lastIndexOf(" ") - 1;
+    }
+}
