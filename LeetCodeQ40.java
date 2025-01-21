74fac0aebad156f941839dd932bad581b2c7a517
iyashreyar <yashreyar5143@gmail.com>
2025-01-21 10:28:30 +0530

    New leetcode ques solved

---
+++ LeetCodeQ40.java
@@ -0,0 +1,20 @@
+
+//ques - 
+
+public class LeetCodeQ40 {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverseWords(s));
+    }
+    static String reverseWords(String s) {
+        StringBuilder t = new StringBuilder();
+        String[] arr = s.trim().split("\\s+");
+        for (int i = arr.length-1; i >= 0; i--) {
+            t.append(arr[i]);
+            if (i != 0){
+                t.append(" ");
+            }
+        } 
+        return t.toString();
+    }
+}
