ab1256c4f84091e095490e3d72b680dc432d125f
iyashreyar <yashreyar5143@gmail.com>
2025-01-30 16:42:19 +0530

    New Leetcode ques added

---
+++ LeetCodeQ44.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class LeetCodeQ44{
+    public static void main(String[] args) {
+        int[] nums1 = {0};
+        int m = 0;
+        int[] nums2 = {1};
+        int n = 1;
+        merge(nums1, m, nums2, n);
+    }
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m, j = 0; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
